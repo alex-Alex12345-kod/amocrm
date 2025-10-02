a6bee0eaf0fe2530efdafa75b75f277b8505a9d6
alex-Alex12345-kod <panda.set@mail.ru>
2025-10-02 14:14:49 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("✅ Сервер AmoCRM работает!");
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
 
